df33b4e6ba0b589ec1a9b3e8e12b05347fabb996
david-graber <75260307+david-graber@users.noreply.github.com>
2020-12-08 10:33:04 -0500

    styling MainNav

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import './App.css';
 import { MainNav } from './components/Nav';
 import ContactList from './components/ContactList';
 
+//Bootstrap Imports
+import Container from 'react-bootstrap/Container';
+
 
 export const App = () => {
   return (
     <div className="App">
-      <MainNav />
-      <ContactList />
+      <Container fluid="md">
+        <MainNav />
+        <ContactList />
+      </Container>
     </div>
   );
 }
